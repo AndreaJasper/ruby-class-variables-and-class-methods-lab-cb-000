5f1e654ffbad4a02a6e4d4926c13ee9583f8bc1f
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-05-13 15:54:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,44 @@
 class Song
-  attr_accessor :name, :artist, :genre
+  
   @@count = 0
   @@genres = []
-  def initialize
-    @@count += 1
-    @@genres += 1
-  end
+  @@artists = []
 
+  attr_accessor :name, :artist, :genre
+  def initialize(name, artist, genre)
+    @name = name
+    @genre = genre
+    @artist = artist
+    @@count += 1
+    @@artist << artist
+    @@genres << genre
+  end
+  
   def self.count
     @@count
   end
+
+  def self.genres
+    genres = []
+    @@genres.each do |genre|
+      if genres.include? genre
+        genre
+      else
+        genres << genre
+      end
+  end
+  genres
+  end
+
+  def self.artists
+    artists = []
+    @@artists.each do |artist|
+      if artists.include? artist
+        artist
+      else
+        artists << artist
+      end
+  end
+  artist
+  end  
 end
