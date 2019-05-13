b5ca6d0c402056daa0f1af0d512765fd1af97dde
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-05-13 15:42:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,13 @@
 class Song
   attr_accessor :name, :artist, :genre
 
-  def initialize=(name, artist, genre)
+  def initialize=(new)
     @name = name
     @artist = artist
     @genre = genre
+  end
+
+  def songs
+
   end
 end
