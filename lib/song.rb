f3bb340e5683d77d25e83af4dbaac0ca7e76cfe9
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-05-13 19:18:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,7 @@ class Song
   artists
   end
 
-  def genre_count
+  def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
     if genre_count.include? genre
@@ -54,7 +54,7 @@ class Song
   genre_count
   end
 
-  def artist_count
+  def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
     if artist_count.include? artist
